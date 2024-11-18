e08d361ce77cd132b32d4c24db7e564961e008f2
Md Alkama <120254460+mdalkama@users.noreply.github.com>
2024-11-19 00:08:07 +0530

    Hint image updated and Verify page updated and VerifyForm merged with Verify and VerifyForm deleted

--- geekymacet/src/components/Verify/Verify.jsx
+++ geekymacet/src/components/Verify/Verify.jsx
@@ -1,46 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react'
+import { IoIosInformationCircleOutline } from "react-icons/io";
 import axios from 'axios';
-import VerifyForm from './VerifyForm';
+import hint from '../../assets/hint.001.jpeg'
 
 function Verify() {
-  const [rollNumber, setRollNumber] = useState('');
-  const [year, setYear] = useState('');
-  const [responseMessage, setResponseMessage] = useState('');
 
-  const handleVerify = async () => {
-    if (!rollNumber || !year) {
-      alert('Roll number and year are required');
-      return;
-    }
-    const rollString = rollNumber.toString();  
-    const yearString = year.toString();        
-    console.log('Sending Data:', { user_roll: rollString, user_year: yearString });
+const [roll, setRoll] = useState('');
+const [rollPre, setRollPre] = useState('');
+const [rollPost, setRollPost] = useState('');
+const [showHint, setShowHint] = useState(false);
 
-  
+// roll update when we change rollpre and rollpost
+useEffect(() => {
+    setRoll(rollPre + rollPost);
+},[rollPre, rollPost]);
+
+const handleVerify = async () => {
+    const rollString = roll.toString();       
     console.log('rollString:', rollString);  
-    console.log('yearString:', yearString);  
-  
     try {
-      const response = await axios.post('http://127.0.0.1:5000/verify', {
+    const response = await axios.post('http://127.0.0.1:5000/verify', {
         user_roll: rollString,  
-        user_year: yearString,  
-      }, {
+    }, {
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         }
-      });
-  
-      setResponseMessage(response.data.message); 
+        
+    });
+    console.log(response.data.message); 
     } catch (error) {
-      setResponseMessage(error.response?.data?.message || 'An error occurred while verifying');
+        alert('An error occurred: ' + error.message);
     }
-  };
-  
-  
+};
 
-  return (
-    <VerifyForm/>
-  );
+return (
+    <div className='py-[10px] mt-[68px] flex h-[calc(100vh-68px-68px)] items-center justify-center'>
+        <div className='bg-[#eaeaf0] px- py-14 flex flex-col items-center justify-center min-w-[300px] max-w-[95%] rounded-lg relative'>
+            <div>
+                <p className='text-blue-950 font-bold text-xl text-nowrap min-w-[300px] max-w-[60%] text-start'>Verify Geeky Macet Members</p>
+                <p className=' pt-1 flex items-center text-orange-400 font-normal text-l min-w-[300px] max-w-[60%] text-start' 
+                    >What is this?&nbsp;<IoIosInformationCircleOutline 
+                    onMouseEnter={() => setShowHint(true)}
+                    onMouseLeave={() => setShowHint(false)}/></p>
+                <div className={`absolute right-0 bg-[#ffffff] shadow-lg rounded-md py-2 transform transition-all duration-300 ease-in-out ${
+                    showHint ? 'opacity-100 pointer-events-auto' : 'opacity-0  pointer-events-none'
+                    }`}>
+                    <img className='min-w-[300px] max-w-[100%] ' src={hint} alt="Hint For Roll" />
+                </div>
+            </div>
+            <form action="/verify" method='post'
+            onSubmit={handleVerify}
+            className='mt-8 flex flex-col items-center justify-center grow h-[100%] min-w-[300px] max-w-[60%]'>
+                <p className='text-black font-bold text-s text-start w-full'>ENTER ROLL NO. TO VERIFY MEMBER</p>
+                    
+                    {/* rollNo input box */}
+                    <div className='mt-4 flex w-full justify-between items-center'>
+                        {/* roll pre */}
+                    <input
+                    value={rollPre} 
+                    onChange={(e)=>{
+                        setRollPre(e.target.value);
+                    }} 
+                    required 
+                    type="number"
+                    id='rollPre'  
+                    name='rollPre'
+                    placeholder='014' 
+                    className='w-[40%] h-[42px] border-[1px] border-gray-300 p-2 rounded-md focus:outline-none focus:border-green-900'/>
+                    <p className='text-3xl'>/</p>
+                      {/* roll post */}
+                    <input
+                    value={rollPost} 
+                    onChange={(e)=>{
+                        setRollPost(e.target.value);
+                    }} 
+                    required 
+                    type="number"
+                    id='rollPost'  
+                    name='rollPost'
+                    placeholder='22' 
+                    className='w-[40%] h-[42px] border-[1px] border-gray-300 p-2 rounded-md focus:outline-none focus:border-green-900'/>
+                    </div>
+                    <button type='submit' className='mt-4 bg-green-950 h-[50px] text-white p-2 w-[100%] rounded-xl font-normal'>Verify</button>
+            </form>
+        </div>
+    </div>
+)
 }
 
 export default Verify;
